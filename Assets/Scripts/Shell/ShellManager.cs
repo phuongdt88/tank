c0a1248f6e8c9975becea85b13885ac50cd9f100
phuongdt88 <phuongdt.88@gmail.com>
2015-11-10 12:58:47 +0700

    alks

---
+++ Assets/Scripts/Shell/ShellManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShellManager : MonoBehaviour {
+
+
+	public float speed = 50f;
+	Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = transform.forward * speed;
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
